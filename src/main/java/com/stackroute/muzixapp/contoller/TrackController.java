2c4bbd16dadfe1c36fe84ace63f6fc4f48927cbf
Nikhila Talla <45334443+tls97nikhila@users.noreply.github.com>
2019-08-07 10:26:44 +0530

    uri mapping updated

--- src/main/java/com/stackroute/muzixapp/contoller/TrackController.java
+++ src/main/java/com/stackroute/muzixapp/contoller/TrackController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "muzixapp/v1")
+@RequestMapping(value = "api/v1")
 public class TrackController {
     MuzixService muzixService;
 
