bc735f3226acae69ab0ce936f3e51c842cbd629c
Nikhila Talla <45334443+tls97nikhila@users.noreply.github.com>
2019-08-08 17:09:01 +0530

    encapsulation

--- src/main/java/com/stackroute/muzixapp/domain/Track.java
+++ src/main/java/com/stackroute/muzixapp/domain/Track.java
@@ -14,8 +14,8 @@ import javax.persistence.Id;
 
 public class Track {
     @Id
-    int id;
-    String trackName;
-    String trackComments;
+   private  int id;
+   private  String trackName;
+   private  String trackComments;
 }
 
