7ef8d3e5674b1654c4a07a15c767afa80b1dff51
Nikhila Talla <45334443+tls97nikhila@users.noreply.github.com>
2019-08-08 11:03:15 +0530

    Update MuzixServiceImpl.java

--- src/main/java/com/stackroute/muzixapp/service/MuzixServiceImpl.java
+++ src/main/java/com/stackroute/muzixapp/service/MuzixServiceImpl.java
@@ -36,9 +36,7 @@ public class MuzixServiceImpl implements MuzixService , ApplicationListener<Cont
     public MuzixServiceImpl(MuzixRepository muzixRepository){
         this.muzixRepository=muzixRepository;
     }
-
-
-    @Override
+ @Override
     public Track saveTrack(Track track)  throws TrackAlreadyExistsException {
         if(muzixRepository.existsById(track.getId())){
             throw new TrackAlreadyExistsException("User already exists!");
@@ -49,42 +47,43 @@ public class MuzixServiceImpl implements MuzixService , ApplicationListener<Cont
         }
         return savedTrack;
     }
-@Override
-    public boolean deleteById(int id) throws TrackNotFoundException {
-        Optional<Track> userId = muzixRepository.findById(id);
-       if(!userId.isPresent()){
+    @Override
+    public List<Track> deleteById(int id) throws TrackNotFoundException {
+        Optional<Track> tracks = muzixRepository.findById(id);
+        if(tracks.isEmpty()){
             throw new TrackNotFoundException("Track not found!");
         }
         muzixRepository.deleteById(id);
-        return true;
+        return  muzixRepository.findAll();
+    }
+
+    public Track getTrackById(int trackId) throws TrackNotFoundException{
+        if(!muzixRepository.existsById(trackId)){
+            throw new TrackNotFoundException("Track Not Found");
+        }
+        Track track1 = muzixRepository.findById(trackId).get();
+        if(track1==null){
+            throw new TrackNotFoundException("Track Not Found");
+        }
+        return track1;
     }
 
     @Override
-    public List<Track> getByName(String name) {
-        List<Track> userId= muzixRepository.findTitleByName(name);
-
-        return userId;
-    }
-
-    @Override
-    public Track updateById(Track track, int id) thorws TrackNotFoundException {
+    public Track updateById(Track track, int id) throws TrackNotFoundException{
         Optional<Track> trackOptional = muzixRepository.findById(id);
 
-        if(!trackOptional.isPresent()){
-           throw new TrackNotFoundException("Track not found!");
-        }
-        
-
-        track.setId(id);
+        if(!trackOptional.isPresent())
+            track.setId(id);
         muzixRepository.save(track);
-        return  trackOptional.get();
+        MuzixService muzixService=null;
+
+        return muzixService.getTrackById(id);
     }
 
     @Override
-    public List<Track> getAllTracks() {
+    public List<Track> getAllTracks() throws TrackNotFounfException{
         return muzixRepository.findAll();
     }
-
 
     // For Application Listner
     @Override
